434a4bba4453b9654c7d9c89c1fa10dedeedefc8
chet <amjoshi6@gmail.com>
2026-02-25 21:47:59 +0530

    Add triangle type classification logic

---
+++ traingle.js
@@ -0,0 +1,31 @@
+//Calculate triange type
+
+let a = 75
+let b = 75
+let c= 30
+
+if (a === b && b===c ){
+    console.log("TRaingle is a equilaterlal triangle");}
+else if (a===b || b===c || c===a){
+    console.log("TRaingle is isolisces triangle");}
+else
+    console.log("Trangle is scalen")
+
+
+a=75; b= 75 ; c=75
+
+if (a === b && b===c ){
+    console.log("TRaingle is a equilaterlal triangle");}
+else if (a===b || b===c || c===a){
+    console.log("TRaingle is isolisces triangle");}
+else
+    console.log("Trangle is scalen")
+
+a=1;b=90; c=99;
+if (a === b && b===c ){
+    console.log("TRaingle is a equilaterlal triangle");}
+else if (a===b || b===c || c===a){
+    console.log("TRaingle is isolisces triangle");}
+else
+    console.log("Trangle is scalen")
+
